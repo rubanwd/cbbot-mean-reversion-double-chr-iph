548cf3daf8472a3c91a81abbd730e5bce22c7b86
luminar-soft <rubanwd@gmail.com>
2024-09-29 12:50:39 +0300

    Double

--- trading_bot.py
+++ trading_bot.py
@@ -39,41 +39,62 @@ class TradingBot:
         logging.basicConfig(filename='trading_bot.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
         
     def job(self):
-
         print("--------------------")
 
-        is_open_positions = self.data_fetcher.get_open_positions(self.symbol)
-        if is_open_positions:
-            print("There is already an open position. A new order will not be placed.")
-            return
-        
+        open_positions = self.data_fetcher.get_open_positions(self.symbol)
+        if open_positions:
+            # Check the number of open positions
+            print(f"Number of open positions: {len(open_positions)}")
+            
+            # If there are 2 or more positions, don't place a new order
+            if len(open_positions) >= 2:
+                print("There are already 2 or more open positions. A new order will not be placed.")
+                return
+            
+            # If there's only 1 open position, check if it was opened more than 3 hours ago
+            if len(open_positions) == 1:
+                open_position = open_positions[0]
+                position_open_time = int(open_position['updatedTime']) / 1000  # Convert ms to seconds
+                current_time = time.time()
+                time_since_open = current_time - position_open_time
+                hours_since_open = time_since_open / 3600
+                
+                print(f"Time since open position: {hours_since_open:.2f} hours")
 
+                # If position is less than 3 hours old, don't place a new order
+                if hours_since_open < 3:
+                    print("Open position is less than 3 hours old. A new order will not be placed.")
+                    return
+        
+        # Check if there was a recently closed position (within the last minute)
+        last_closed_position = self.data_fetcher.get_last_closed_position(self.symbol)
+        if last_closed_position:
+            last_closed_time = int(last_closed_position['updatedTime']) / 1000
+            current_time = time.time()
+            time_since_last_close = current_time - last_closed_time
+            print(f"Time since last closed position: {int(time_since_last_close)} seconds")
+
+            # Prevent new order if the last position was closed less than 1 minute ago
+            if time_since_last_close < 120:
+                print("The last closed position was less than 1 minute ago. A new order will not be placed.")
+                return
+
+        # Check if there are open limit orders
         is_open_orders = self.data_fetcher.get_open_orders(self.symbol)
         if is_open_orders:
             print("There is an open limit order. A new order will not be placed.")
             return
-        
+
+        # Fetch historical data for the strategy
         get_historical_data = self.data_fetcher.get_historical_data(self.symbol, self.interval, self.limit)
         if get_historical_data is None:
             print("Failed to retrieve historical data.")
             return
 
         df = self.strategy.prepare_dataframe(get_historical_data)
-        
         trend = self.strategy.mean_reversion_strategy(df)
+        
         if trend:
-
-            last_closed_position = self.data_fetcher.get_last_closed_position(self.symbol)
-            if last_closed_position:
-                last_closed_time = int(last_closed_position['updatedTime']) / 1000
-                current_time = time.time()
-                time_since_last_close = current_time - last_closed_time
-                print(f"Time since last closed position: {int(time_since_last_close)} seconds")
-                if time_since_last_close < 120:
-                    print("The last closed position was less than 1 minute ago. A new order will not be placed.")
-                    return
-                
-            
             stop_loss, take_profit = self.risk_management.calculate_dynamic_risk_management(df, trend)
 
             print(f"Trend: {trend.upper()}")
@@ -101,6 +122,8 @@ class TradingBot:
                 print("Failed to place order.")
         else:
             print("No suitable signals for position opening.")
+
+
 
     def run(self):
         self.job()
